4f5757e275d0e68966fae849bbe6c9fd241ae17b
johnnysuzuki <amirak2501@gmail.com>
2022-02-25 02:11:59 +0900

    敵の方向転換

--- new johnny's sandbox/Assets/MetroidvaniaTools/scripts/AIScripts/EnemyCharacter.cs
+++ new johnny's sandbox/Assets/MetroidvaniaTools/scripts/AIScripts/EnemyCharacter.cs
@@ -10,6 +10,7 @@ namespace MetroidvaniaTools
         public bool facingLeft;
         protected Rigidbody2D rb;
         protected Collider2D col;
+        protected int rayHitNumber;
 
 
         // Start is called before the first frame update
@@ -22,6 +23,42 @@ namespace MetroidvaniaTools
         {
             rb = GetComponent<Rigidbody2D>();
             col = GetComponent<Collider2D>();
+        }
+
+        protected virtual bool CollisionCheck(Vector2 direction, float distance, LayerMask collision)
+        {
+            RaycastHit2D[] hits = new RaycastHit2D[10];
+            int numHits = col.Cast(direction, hits, distance);
+            for (int i = 0; i < numHits; i++)
+            {
+                if ((1 << hits[i].collider.gameObject.layer & collision) != 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected virtual void CheckGround()
+        {
+            Ray2D[] groundRays = new Ray2D[3];
+            groundRays[0].origin = new Vector2(transform.position.x - (transform.localScale.x * .5f), transform.position.y - .05f);
+            groundRays[1].origin = new Vector2(transform.position.x , transform.position.y - .05f);
+            groundRays[2].origin = new Vector2(transform.position.x + (transform.localScale.x * .5f), transform.position.y - .05f);
+            RaycastHit2D[] hits = new RaycastHit2D[3];
+            for(int i = 0; i<3; i++)
+            {
+                hits[i] = Physics2D.Raycast(groundRays[i].origin, -transform.up, Mathf.Abs(transform.localScale.x * .5f));
+            }
+            int numberOfHits = 0;
+            foreach(RaycastHit2D hit in hits)
+            {
+                if (hit)
+                {
+                    numberOfHits++;
+                }
+            }
+            rayHitNumber = numberOfHits;
         }
     }
 }
--- new johnny's sandbox/Assets/MetroidvaniaTools/scripts/AIScripts/EnemyMovement.cs
+++ new johnny's sandbox/Assets/MetroidvaniaTools/scripts/AIScripts/EnemyMovement.cs
@@ -11,24 +11,47 @@ namespace MetroidvaniaTools
         [SerializeField]
         protected MovementType type;
         [SerializeField]
+        protected bool spawnFacingLeft;
+        [SerializeField]
+        protected bool turnAroundOnCollision;
+        [SerializeField]
+        protected bool avoidFalling;
+        [SerializeField]
         protected float timeTillMaxSpeed;
         [SerializeField]
         protected float MaxSpeed;//最高速
+        [SerializeField]
+        protected LayerMask collidersToTurnAroundOn;
 
+        private bool spawning = true;
         private float acceleration;//加速度
         private float currentSpeed;//現在の速度
         private float direction;//向き
         private float runTime;//走っている時間
         private float originalGravityForceMultiplier;//最初の重力
 
+        protected bool wait;
+        protected float originalWaitTime = .1f;
+        protected float currentWaitTime;
+
         protected override void Initialization()
         {
             base.Initialization();
+            if (spawnFacingLeft)
+            {
+                enemyCharacter.facingLeft = true;
+                transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+            }
+            currentWaitTime = originalWaitTime;
+            Invoke("Spawning", .01f);
         }
 
         protected virtual void FixedUpdate()
         {
             Movement();
+            CheckGround();
+            EdgeOfFloor();
+            HandleWait();
         }
 
         protected virtual void Movement()
@@ -36,10 +59,20 @@ namespace MetroidvaniaTools
             if (!enemyCharacter.facingLeft)
             {
                 direction = 1;
+                if (CollisionCheck(Vector2.right, .5f, collidersToTurnAroundOn) && turnAroundOnCollision && !spawning)
+                {
+                    enemyCharacter.facingLeft = true;
+                    transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+                }
             }
             else
             {
                 direction = -1;
+                if (CollisionCheck(Vector2.left, .5f, collidersToTurnAroundOn) && turnAroundOnCollision && !spawning)
+                {
+                    enemyCharacter.facingLeft = false;
+                    transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+                }
             }
             acceleration = MaxSpeed / timeTillMaxSpeed;
             runTime += Time.deltaTime;
@@ -58,7 +91,32 @@ namespace MetroidvaniaTools
             {
                 currentSpeed = -MaxSpeed;
             }
+        }
 
+        protected virtual void EdgeOfFloor()
+        {
+            if(rayHitNumber == 1 && avoidFalling && !wait)
+            {
+                currentWaitTime = originalWaitTime;
+                wait = true;
+                enemyCharacter.facingLeft = !enemyCharacter.facingLeft;
+                transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+            }
+        }
+
+        protected virtual void Spawning()
+        {
+            spawning = false;
+        }
+
+        protected virtual void HandleWait()
+        {
+            currentWaitTime -= Time.deltaTime;
+            if(currentWaitTime <= 0)
+            {
+                wait = false;
+                currentWaitTime = 0;
+            }
         }
     }
 }
